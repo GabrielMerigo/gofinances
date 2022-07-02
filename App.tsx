5cba53210c8ec95347ee2a53ffb2ddbcc8cd0be4
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-07-01 22:41:33 -0300

    using styled-components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Dashboard } from './src/screens/Dashboards';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Teste Nelson</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Dashboard />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
