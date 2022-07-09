85a89694bf199a1358c620a57c8074746cfa5d36
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-07-08 22:55:30 -0300

    regsiter

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+
+import * as S from "./styles"
+
+export function Register() {
+  return (
+    <S.Container>
+      <Text>
+        Register
+      </Text>
+    </S.Container>
+  );
+}
